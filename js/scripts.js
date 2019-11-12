8b7183eb4fa55b702646033b265ea1e9fa051aa8
Adilet312 <adiletm@hotmail.com>
2019-11-11 22:23:50 -0800

    Final project about Order Pizza.

--- js/scripts.js
+++ js/scripts.js
@@ -1,93 +1,131 @@
-                      /*Backend logic*/
-/*Class name  is Pizza and it has two instance variables*/
-function Pizza(typePizza,size){
-  this.typePizza = typePizza;
-  this.size = size;
+
+/* Pizza class with two instance variables. */
+function Pizza(type_pizza,size_pizza){
+  this.type_pizza = type_pizza;
+  this.size_pizza = size_pizza;
 }
-/*getPizza() method returns pizza type  and size by concatenating.*/
-Pizza.prototype.getPizza=function(){
-  return ("Pizza type: "+this.typePizza+", Size of the pizza: "+this.size);
-}
-/*totalPrice() returns total price based on user's order*/
-Pizza.prototype.totalPrice=function(){
-  if(this.typePizza==="Cheese" && this.size==='small'){return 20}
-  else if(this.typePizza==="Cheese" && this.size==='medium'){return 30}
-  else if(this.typePizza==="Cheese" && this.size==='large'){return 40}
-  else if(this.typePizza==="Pepperoni" && this.size==='small'){return 30}
-  else if(this.typePizza==="Pepperoni" && this.size==='medium'){return 40}
-  else if(this.typePizza==="Pepperoni" && this.size==='large'){return 50}
-  else if(this.typePizza==="Artichoke" && this.size==='small'){return 25}
-  else if(this.typePizza==="Artichoke" && this.size==='medium'){return 30}
-  else if(this.typePizza==="Artichoke" && this.size==='large'){return 35}
-  else if(this.typePizza==="Anchovy" && this.size==='small'){return 25}
-  else if(this.typePizza==="Anchovy" && this.size==='medium'){return 35}
-  else if(this.typePizza==="Anchovy" && this.size==='large'){return 45}
-}
-/*Class name is PizzaList that stores all pizzas from user.*/
-function PizzaList(){
-  this.list = [];
-  this.currentId = 0;
-}
-/* addPizza(parameter) adds Pizzas into list.*/
-PizzaList.prototype.addPizza=function(pizza){
-  pizza.id = this.assignId();
-  this.list.push(pizza);
-}
-/*assignId() method keeps track it's index number of the Pizzas*/
-PizzaList.prototype.assignId=function(){
-  this.currentId+=1;
-  return this.currentId;
-}
-/*getSize() returns size of the list*/
-PizzaList.prototype.getSize = function(){
-    return this.currentId;
-}
-/*findPizzaById(parameter) method to find pizza by its index number.*/
-PizzaList.prototype.findPizzaById=function(idx){
-  for(var i=0; i<this.list.length; i++){
-    if(this.list[i].id===idx){
-      return this.list[i];
-    }
+Pizza.prototype.pizzaProperties=function(){
+  if(this.type_pizza==='cheesePizza'){
+      return ("Type of pizza: Cheese Pizza, Size of pizza: "+this.size_pizza+'\n')
+  }
+  else if(this.type_pizza==='pepperoniPizza'){
+      return ("Type of pizza: Pepperonni Pizza, Size of pizza: "+this.size_pizza+'\n')
+  }
+  else if(this.type_pizza==='artichokePizza'){
+      return ("Type of pizza: Artichoke Pizza, Size of pizza: "+this.size_pizza+'\n')
+  }
+  else{
+      return " ";
   }
 }
+Pizza.prototype.prize = function(){
+  if(this.type_pizza==='cheesePizza' && this.size_pizza==='small'){return 20;}
+  else if(this.type_pizza==='cheesePizza' && this.size_pizza==='medium'){return 30;}
+  else if(this.type_pizza==='cheesePizza' && this.size_pizza==='large'){return 40;}
+  else if(this.type_pizza==='pepperoniPizza' && this.size_pizza==='small'){return 25;}
+  else if(this.type_pizza==='pepperoniPizza' && this.size_pizza==='medium'){return 30;}
+  else if(this.type_pizza==='pepperoniPizza' && this.size_pizza==='large'){return 35;}
+  else if(this.type_pizza==='artichokePizza' && this.size_pizza==='small'){return 40;}
+  else if(this.type_pizza==='artichokePizza' && this.size_pizza==='medium'){return 50;}
+  else if(this.type_pizza==='artichokePizza' && this.size_pizza==='large'){return 60;}
+}
+/**Class ListPizza to store all ordered pizzas */
+function ListPizza(){
+  this.list = [];
+  this.currentID = 0;
+}
+ListPizza.prototype.addPizza=function(pizza){
+  pizza.id = this.assignID();
+  this.list.push(pizza);
+}
+ListPizza.prototype.assignID=function(){
+  this.currentID+=1;
+  return this.currentID;
+}
+ListPizza.prototype.findPizzaById=function(idx){
+  for(let index=0; index<this.list.length; index++){
+      if(this.list[index]){
+          if(this.list[index].id===idx){
+              return this.list[index];
+          }
+      }
+  }
+  return null;
 
-/*Fondend logic*/
-var pizzalist = new PizzaList();//ref
-$(document).ready(function()
-{
-  $("#formID").submit(function(event)
+}
+ListPizza.prototype.deletePizzaById=function(idx){
+  for(let index=0; index<this.list.length; index++){
+      if(this.list[index]){
+          if(this.list[index].id===idx){
+              delete this.list[index];
+              return true;
+          }
+      }
+  }
+  return false;
+
+}
+/*Frontend logic*/ 
+
+let totalAmount = 0;
+let output = "";
+let listpizza = new ListPizza();
+function addOrders(){
+  let type_pizza = document.getElementById("pizzaTypeID").value;
+  let size_pizza = document.getElementById("sizeID").value;
+  //Data validation 
+  if((type_pizza==='cheesePizza'|| type_pizza==='pepperoniPizza' || type_pizza==='artichokePizza') && 
+  (size_pizza==='small'||size_pizza==='medium'||size_pizza==='large'))
   {
-    event.preventDefault();
-    var inputPizzaToppings = [];
-    var index=0;
-   $("input:checkbox[name=pizza]:checked").each(function()
-    {
-        inputPizzaToppings[index] = $(this).val();
-        index++;
-    });
-    var inputSize = $("#sizeID").val();
+      pizza = new Pizza(type_pizza,size_pizza);
+      console.log(type_pizza,size_pizza);
+      listpizza.addPizza(pizza);
+      
+  }
+  else
+  {
+      if(type_pizza==="" && size_pizza==="")
+      {
+          $("#output").text("Please choose type  and size of the pizza:").fadeIn();
+          
+      }
+      else if((type_pizza=="")&& (size_pizza==='small'||size_pizza==='medium'||size_pizza==='large'))
+      {
+          $("#output").text("Please choose type of the pizza:").fadeIn();
+      }
+      else
+      {
+          $("#output").text("Please choose size of the pizza:").fadeIn();
+      }
 
-    for(var idx = 0; idx < inputPizzaToppings.length; idx++){
-      pizzalist.addPizza(new Pizza(inputPizzaToppings[idx],inputSize));
-    }
-    var total_amount = 0;
-    for(var index=1; index<=pizzalist.getSize();index++){
-      total_amount+=pizzalist.findPizzaById(index).totalPrice();
+  }
+  
 
-    }
+  
+  
+}
 
-    var result = "Total price: $"+total_amount+'\n'
-                 +"Number of pizzas: "+pizzalist.getSize()+'\n'
-                 +"Size of pizzas: "+inputSize;
-    $("#output").text(result);
-    console.log(pizzalist.getSize())
+function placeOrder(){
+
+ 
+  for( let i=1;i<=listpizza.currentID; i++){
+      output+=listpizza.findPizzaById(i).pizzaProperties()+", Price is $"+listpizza.findPizzaById(i).prize()+';\n';
+      totalAmount+=listpizza.findPizzaById(i).prize();
+  }
+
+  $("#output").text(output).fadeIn();
+  console.log(addressAndName());
+  $("#outputPrice").text(addressAndName()+";  Total Price: $"+totalAmount).show();
+
+  
+  
+}
+function addressAndName(){
+  let inputName = document.getElementById("fullNameID").value;
+  let inputAddress = document.getElementById("addressID").value;
+  return ("Full name: "+inputName+", Address: "+inputAddress);
+  
+}
 
 
 
-
-
-
-
-
-  });
-});
